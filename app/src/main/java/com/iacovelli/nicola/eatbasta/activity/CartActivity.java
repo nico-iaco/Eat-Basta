3ec59560eb6c38a68dfc128326f31d78f1665aff
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2019-02-10 12:59:57 +0100

    Restyling CartActivity adding more material design (issue #6)
    Signed-off-by: Nicola Iacovelli <nicolaiacovelli98@gmail.com>

--- app/src/main/java/com/iacovelli/nicola/eatbasta/activity/CartActivity.java
+++ app/src/main/java/com/iacovelli/nicola/eatbasta/activity/CartActivity.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -31,6 +32,7 @@ public class CartActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ProgressBar progress;
     private TextView totalTxt;
+    private TextView minOrderTxt;
     private Button checkoutBtn;
     private float minimumOrder = 40.4f;
 
@@ -42,9 +44,11 @@ public class CartActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.product_list);
         progress = findViewById(R.id.checkout_bar);
         progress.setIndeterminate(false);
-        progress.setMax((int) minimumOrder);
+        progress.setMax((int) (minimumOrder * 100));
         progress.setProgress(0);
         totalTxt = findViewById(R.id.total_text);
+        minOrderTxt = findViewById(R.id.restaurant_min_order_cart);
+        minOrderTxt.setText(String.valueOf(minimumOrder));
         checkoutBtn = findViewById(R.id.checkout_button);
         CustomViewModel model = ViewModelProviders.of(this).get(CustomViewModel.class);
         model.getProducts().observe(this, p -> {
@@ -82,10 +86,13 @@ public class CartActivity extends AppCompatActivity {
     @TargetApi(Build.VERSION_CODES.N)
     private void checkProducts(List<Product> productList) {
         double total = 0;
+        Log.d("Dimensione: ", String.valueOf(productList.size()));
+        //productList.removeIf(p -> p.getProductQuantity()==0);
+        //Log.d("Dimensione: ", String.valueOf(productList.size()));
         for (Product p : productList) {
             total += (p.getProductPrice() * p.getProductQuantity());
         }
-        progress.setProgress((int) Math.round(total), true);
+        progress.setProgress((int) Math.round(total * 100), true);
         DecimalFormat df = new DecimalFormat("###.##");
         String totalString = df.format(total);
         totalTxt.setText(totalString);
--- app/src/main/java/com/iacovelli/nicola/eatbasta/viewmodel/CustomViewModel.java
+++ app/src/main/java/com/iacovelli/nicola/eatbasta/viewmodel/CustomViewModel.java
@@ -5,7 +5,6 @@ import com.iacovelli.nicola.eatbasta.model.Product;
 import com.iacovelli.nicola.eatbasta.model.Restaurant;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
@@ -33,20 +32,22 @@ public class CustomViewModel extends ViewModel {
     }
 
     private void loadRestaurants() {
-        ArrayList<Restaurant> restaurantArrayList = new ArrayList<>(Arrays.asList(
-                new Restaurant("Panucci's pizza", "The best pizza ever", R.drawable.restaurant, 10),
-                new Restaurant("Calabrese", "Mannaia a tia", R.drawable.restaurant, 15),
-                new Restaurant("Burger king", "Tutti i miei amici vanno a burger king", R.drawable.restaurant, 20),
-                new Restaurant("Mensa elis", "Ogni giorno surprise", R.drawable.restaurant, 50)
-        ));
+        List<Restaurant> restaurantArrayList = new ArrayList<>();
+        restaurantArrayList.add(new Restaurant("Panucci's pizza", "The best pizza ever", R.drawable.restaurant, 10));
+        restaurantArrayList.add(new Restaurant("Calabrese", "Mannaia a tia", R.drawable.restaurant, 15));
+        restaurantArrayList.add(new Restaurant("Burger king", "Tutti i miei amici vanno a burger king", R.drawable.restaurant, 20));
+        restaurantArrayList.add(new Restaurant("Mensa elis", "Ogni giorno surprise", R.drawable.restaurant, 50));
         restaurants.setValue(restaurantArrayList);
     }
 
     private void loadProducts() {
-        List<Product> productList = new ArrayList<>(Arrays.asList(
-                new Product("Iphone", 15.5f),
-                new Product("Mammt", 18.4f)
-        ));
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("Iphone", 15.5f));
+        productList.add(new Product("Mammt", 18.4f));
+        productList.add(new Product("Onion", 1));
+        productList.add(new Product("Lucidamario", 6.2f));
+        productList.add(new Product("Mela", 0.4f));
+        productList.add(new Product("Pera", 1.4f));
         products.setValue(productList);
     }
 }
